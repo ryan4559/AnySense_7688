abd7b26996a7ad99475565951722ee47352d64f9
root <ryan4559@gmail.com>
2017-10-23 18:51:22 +0000

    fix git pull bug in init.py

--- init.py
+++ init.py
@@ -4,6 +4,10 @@ import subprocess
 
 print('[System]Start')
 
+#fix git-pull(once)
+print('[System]fix git-pull bug')
+subprocess.call('ln -s /usr/bin/git /usr/lib/git-core/git', shell=Ture)
+
 #sync system time
 subprocess.call('python ntp.py', shell=True)
 
